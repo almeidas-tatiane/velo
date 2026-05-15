1c037a40e108a4b04d079e64034f42a4781b4e2d
almeidas-tatiane <almeidas.tatiane@gmail.com>
2026-05-15 11:56:38 -0300

    meu primeiro commit com git

---
+++ playwright/e2e/online.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+test('webapp deve estar on line', async ({ page }) => {
+  await page.goto('http://localhost:5173')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Velô by Papito/)
+})
+
+
